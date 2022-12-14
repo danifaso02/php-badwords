62b961a3975207269661298078050439f8a366b2
danifaso02 <danifaso02@gmail.com>
2022-12-14 16:10:00 +0100

    fine

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 // dichiarazione paragrafo
 $parag = 'In analisi matematica, l integrale è un operatore lineare che, nel caso di una funzione di una sola variabile a valori reali non negativi, associa alla funzione l area sottesa dal suo grafico entro un dato intervallo nel dominio. Se la funzione assume anche valori negativi, allora l integrale può essere interpretato geometricamente come l area orientata sottesa dal grafico della funzione.';
+
+$censure_word = $_GET['word'] ?? null;
+$text_censured = str_replace($censure_word, '***', $text);
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +15,20 @@ $parag = 'In analisi matematica, l integrale è un operatore lineare che, nel ca
     <title>Bad words</title>
 </head>
 <body>
-    <p><?php echo $text ?></p>
+    <form action="">
+        <label for="censure-word">Parola da censurare</label>
+        <input id="censure-word" type="text" placeholder="Inserisci parola">
+        <button type="submit">Invia</button>
+    </form>
+
+    <hr>
+
+    <h3>Lunghezza: <?php echo strlen($parag); ?></h3>
+    <p><?php echo $parag; ?></p>
+
+    <hr>
+
+    <h3>Lunghezza: <?php echo strlen($text_censured); ?></h3>
+    <p><?php echo $text_censured; ?></p>
 </body>
 </html>
